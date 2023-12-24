f45b7f5e2fe36a557096cd8a567ffc0846140b6a
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-12-24 13:18:09 +0200

    Add functionality to create tasks with graphQL

--- tasks/schema.py
+++ tasks/schema.py
@@ -8,11 +8,28 @@ class TasksType(DjangoObjectType):
         model = Task
 
 
+class CreateTaskMutation(graphene.Mutation):
+    class Arguments:
+        title = graphene.String(required=True)
+        description = graphene.String(required=False)
+
+    task = graphene.Field(TasksType)
+
+    def mutate(self, info, title, description):
+        task = Task(title=title, description=description)
+        task.save()
+        return CreateTaskMutation(task=task)
+
+
 class Query(graphene.ObjectType):
     all_tasks = graphene.List(TasksType)
 
-    def resolve_all_tasks(root, info):
+    def resolve_all_tasks(self, info):
         return Task.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_task = CreateTaskMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
