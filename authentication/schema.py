960a96129c2ae314e899d330680469bdc30aaef3
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-12-30 10:36:29 +0200

    Add user login, update, resend activation email and password functionality

--- authentication/schema.py
+++ authentication/schema.py
@@ -7,6 +7,10 @@ from graphql_auth.schema import UserQuery, MeQuery
 class AuthMutation(graphene.ObjectType):
     register = mutations.Register.Field()
     verify_account = mutations.VerifyAccount.Field()
+    token_auth = mutations.ObtainJSONWebToken.Field()
+    update_account = mutations.UpdateAccount.Field()
+    resend_activation_email = mutations.ResendActivationEmail.Field()
+    send_password_reset_email = mutations.SendPasswordResetEmail.Field()
 
 
 class Query(UserQuery, MeQuery, graphene.ObjectType):
